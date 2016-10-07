852b26ec7bc1beeab57eb6134674070cb30ec744
png1ed <png1ed@gmail.com>
2016-10-07 10:27:09 +0100

    Create VIEW-edit.php

---
+++ sbs/pgr/projects/convert-BR-to-P/VIEW-edit.php
@@ -0,0 +1,10 @@
+// ADD:
+
+CKEDITOR.instances['${ID}SubjectareaId'].setData(pn.__.ent2HTML("${r/subjectarea}"));
+
+
+// REMOVE:
+
+pn.J('textarea#${ID}SubjectareaId').val('${r/subjectarea}' ...
+
+dat['data[${ID}][subjectarea]'] ...
